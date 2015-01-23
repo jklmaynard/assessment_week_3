f8d89247e2f0875a4df16e1130ff3f3d8219ef0e
James <jameskirkmaynard@gmail.com>
2015-01-23 11:21:39 -0800

    Write sinatra code for Stylist get/post methods.

--- app.rb
+++ app.rb
@@ -4,3 +4,18 @@ also_reload('lib/**/*.rb')
 require('./lib/stylist')
 require('./lib/client')
 require('pg')
+
+DB = PG.connect({ :dbname => "salon" })
+
+get("/") do
+  @stylists = Stylist.all()
+  erb(:index)
+end
+
+post("/stylists") do
+  stylist_name = params.fetch("stylist_name")
+  stylist = Stylist.new({ :name => stylist_name, :id => nil })
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:index)
+end
