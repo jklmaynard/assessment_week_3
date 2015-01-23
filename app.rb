39eb0ed605c5da2a4003423f349eadb1429d40fe
James <jameskirkmaynard@gmail.com>
2015-01-23 13:58:51 -0800

    Add stylist.erb file with complementary get/post methods in app.rb.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
+require('pry')
 also_reload('lib/**/*.rb')
 require('./lib/stylist')
 require('./lib/client')
@@ -7,15 +8,29 @@ require('pg')
 
 DB = PG.connect({ :dbname => "salon" })
 
-get("/") do
-  @stylists = Stylist.all()
-  erb(:index)
-end
+  get("/") do
+    @stylists = Stylist.all()
+    erb(:index)
+  end
 
-post("/stylists") do
-  stylist_name = params.fetch("stylist_name")
-  stylist = Stylist.new({ :name => stylist_name, :id => nil })
-  stylist.save()
-  @stylists = Stylist.all()
-  erb(:index)
-end
+  post("/stylists") do
+    stylist_name = params.fetch("stylist_name")
+    stylist = Stylist.new({ :name => stylist_name, :id => nil })
+    stylist.save()
+    @stylists = Stylist.all()
+    erb(:index)
+  end
+
+  get("/stylists/:id") do
+    @stylist = Stylist.find(params.fetch("id").to_i())
+    erb(:stylist)
+  end
+
+  post("/clients") do
+    name = params.fetch("name")
+    stylist_id = params.fetch("stylist_id").to_i()
+    client = Client.new({ :name => name, :stylist_id => stylist_id })
+    client.save()
+    @stylist = Stylist.find(stylist_id)
+    erb(:stylist)
+  end
